9c0ff6749942b1b9ac2b1ed24e2eca0fa7d797cd
syshut <attis28@qq.com>
2024-11-12 17:28:06 +0800

    Update history.sh

--- history.sh
+++ history.sh
@@ -49,21 +49,29 @@ if [ ! -s "$CONFIG_FILE" ]; then
 fi
 
 # Step 3: 替换 server_name
-DOMAIN="chinaaie.cn"
+read -p "请输入域名 (如 example.com): " DOMAIN
+if [ -z "$DOMAIN" ]; then
+  echo "域名不能为空！"
+  exit 1
+fi
 
 # Step 4: 获取 /etc/nginx/conf.d/default.conf 中的 root 指令内容并替换
 DEFAULT_CONF="/etc/nginx/conf.d/default.conf"
 ROOT_PATH=$(grep -E '^ *root ' "$DEFAULT_CONF" | awk '{print $2}' | tr -d ';')
 if [ -n "$ROOT_PATH" ]; then
   sudo sed -i "s|root .*;|root $ROOT_PATH;|" "$CONFIG_FILE"
+  echo "1"
 else
   echo "未找到 root 指令，跳过替换。"
 fi
 
 # Step 5: 修改 ssl_certificate 和 ssl_certificate_key
 sudo sed -i "s|ssl_certificate .*|ssl_certificate /usr/local/etc/xray/ssl/${DOMAIN}.fullchain.cer;|" "$CONFIG_FILE"
+  echo "2"
 sudo sed -i "s|ssl_certificate_key .*|ssl_certificate_key /usr/local/etc/xray/ssl/${DOMAIN}.key;|" "$CONFIG_FILE"
+  echo "3"
 
 # Step 6: 修改 location /你的 ServiceName
 read -p "请输入 ServiceName (如 mygrpc): " SERVICE_NAME
 sudo sed -i "s|location /你的 ServiceName|location /$SERVICE_NAME|" "$CONFIG_FILE"
+  echo "4"
